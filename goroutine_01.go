4db48dc03995854273c3bb7cb4d02838c8218dcc
nikhil.patne <nikhil.patne@gslab.com>
2021-01-14 10:25:10 +0530

    go routine 01

---
+++ goroutine_01.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go count("test") // this works in background, async
+	go count("test1")
+
+	fmt.Scanln()
+}
+
+func count(test string) {
+	for {
+		time.Sleep(time.Second * 1)
+		fmt.Println(test)
+	}
+}
